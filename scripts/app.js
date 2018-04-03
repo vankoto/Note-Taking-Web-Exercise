e870367aff67004a0c4c133506ad0d9df3fb9868
Ivan <ivanvelichkov357@gmail.com>
2018-04-03 21:07:42 +0200

    Comment for the mousedown jquery

--- scripts/app.js
+++ scripts/app.js
@@ -14,7 +14,8 @@ function closeFunction(){
   $('.modal-overlay').fadeOut();
   $('.modal-box').slideUp();
 }
-
+//The link where I got the idea for the login form to close when mouse-down button outside of the form
+//https://api.jquery.com/mousedown/
 $(document).ready(function(){
   $('.modal-overlay').mousedown(function(){
     $('.modal-overlay').fadeOut();
